23bf1cde34dc64227deecd8f12328d3acb80ee01
Theja <gandhap@email.com>
2025-06-29 21:08:51 +0530

    created new file

---
+++ p1.java
@@ -0,0 +1,9 @@
+package theja;
+
+public class p1 {
+     public static void main(String []args)
+    {
+        System.out.println("Hello World");
+         System.out.println("Hello World");
+    }
+}
